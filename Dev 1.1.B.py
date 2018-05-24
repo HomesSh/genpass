c8f70f174053bad4a77c15b4f235cd67768ff7eb
HomesSh <37923829+HomesSh@users.noreply.github.com>
2018-05-24 15:41:51 +0200

    Add files via upload

---
+++ Dev 1.1.B.py
@@ -0,0 +1,41 @@
+from tkinter import*
+
+main = Tk()
+main.title("Projet")
+#main['bg'] = "#3c3c3c"
+main.geometry("300x300")
+
+maj_var = IntVar()
+min_var = IntVar()
+nb_var = IntVar()
+mot_de_passe = StringVar()
+nb_caracteres = StringVar()
+
+
+def printmsg():
+    if maj_var.get() == 1:
+        print("MAJ",end=" | ")
+    if min_var.get() == 1:
+        print("MIN",end=" | ")
+    if nb_var.get() == 1:
+        print("NOMBRES",end=" | ")
+    print(nb_caracteres.get()," caractères")
+
+    
+titre = Label(main, text="Générateur de mots de passe", font='Helvetica 12 bold underline').pack(side=TOP)
+bt = Button(main, text="Générer", command=printmsg,).pack(side=BOTTOM)
+maj_check = Checkbutton(main, text="Majuscules", variable=maj_var).pack(side=TOP)
+min_check = Checkbutton(main, text="Minuscules",variable=min_var).pack(side=TOP)
+nb_check = Checkbutton(main, text="Nombres", variable=nb_var).pack(side=TOP)
+mdp = Label(main, textvariable=mot_de_passe).pack(side=BOTTOM)
+slide_caracteres = Scale(main, from_=1, to=25, orient=HORIZONTAL, variable=nb_caracteres).pack()
+
+
+
+
+
+
+
+
+
+main.mainloop()
